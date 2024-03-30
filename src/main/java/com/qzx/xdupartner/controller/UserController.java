aa89f104bf021de47532a1de1565bec2b83c8d70
qzx0712 <1500418656@qq.com>
2024-03-30 17:59:00 +0800

    aaa

--- src/main/java/com/qzx/xdupartner/controller/UserController.java
+++ src/main/java/com/qzx/xdupartner/controller/UserController.java
@@ -10,6 +10,8 @@ import com.qzx.xdupartner.constant.RedisConstant;
 import com.qzx.xdupartner.constant.SystemConstant;
 import com.qzx.xdupartner.entity.User;
 import com.qzx.xdupartner.entity.vo.BlogVo;
+import com.qzx.xdupartner.entity.vo.ResultCode;
+import com.qzx.xdupartner.entity.vo.ResultVo;
 import com.qzx.xdupartner.entity.vo.UserInfoVo;
 import com.qzx.xdupartner.exception.ApiException;
 import com.qzx.xdupartner.service.BlogService;
@@ -112,7 +114,7 @@ public class UserController {
     }
     @ApiOperation("")
     @PostMapping(value = "/changeUserInfo", produces = "application/json;charset=utf-8")
-    public String ChangeUserInfo(@Validated @RequestBody UserInfoVo userInfoVo) {
+    public ResultVo<String> ChangeUserInfo(@Validated @RequestBody UserInfoVo userInfoVo) {
         User user = transferToUser(userInfoVo);
         userService.updateById(user);
         stringRedisTemplate.opsForValue()
@@ -121,7 +123,7 @@ public class UserController {
                         TimeUnit.HOURS);
         UserHolder.saveUser(userService.getById(UserHolder.getUserId()));
         stringRedisTemplate.delete(RedisConstant.USERVO_CACHE + UserHolder.getUserId());
-        return "修改成功";
+        return new ResultVo<>(ResultCode.SUCCESS,"修改成功");
     }
     @ApiOperation("")
     @PostMapping(value = "/login", produces = "application/json;charset=utf-8")
@@ -199,31 +201,31 @@ public class UserController {
     }
     @ApiOperation("")
     @GetMapping(value = "/me")
-    public UserInfoVo ofMe() {
+    public ResultVo<UserInfoVo> ofMe() {
         User user = userService.getById(UserHolder.getUserId());
-        return getUserInfoVo(user);
+        return new ResultVo<>(ResultCode.SUCCESS,getUserInfoVo(user));
     }
 
     @ApiOperation("")
     @GetMapping(value = "/logout")
-    public String logout() {
+    public ResultVo<String>  logout() {
         UserHolder.removeUser();
         stringRedisTemplate.delete(RedisConstant.LOGIN_PREFIX + UserHolder.getUserId());
-        return "登出成功";
+        return new ResultVo<>(ResultCode.SUCCESS,"登出成功");
     }
     @ApiOperation("")
     @GetMapping(value = "/queryMyselfPub")
-    public List<BlogVo> queryMyselfPub(@RequestParam(value = "current", defaultValue = "1") Integer current) {
-        return blogService.getOnesBlogVo(UserHolder.getUserId(), current);
+    public ResultVo<List<BlogVo>> queryMyselfPub(@RequestParam(value = "current", defaultValue = "1") Integer current) {
+        return new ResultVo<>(ResultCode.SUCCESS,blogService.getOnesBlogVo(UserHolder.getUserId(), current));
     }
     @ApiOperation("")
     @GetMapping(value = "/otherUser/{userId}")
-    public UserInfoVo queryOther(@PathVariable Long userId) {
+    public  ResultVo<UserInfoVo>  queryOther(@PathVariable Long userId) {
         User byId = userService.getById(userId);
         if (byId == null) {
             throw new ApiException("用户不存在！");
         }
-        return getUserInfoVo(byId);
+        return new ResultVo<>(ResultCode.SUCCESS,getUserInfoVo(byId));
     }
 }
 
