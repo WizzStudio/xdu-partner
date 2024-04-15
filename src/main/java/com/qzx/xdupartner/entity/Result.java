bde571cda0d855e28427fe645fb217fa89dccb86
qizhenxin <qizhenxin@dcarlife.com>
2024-04-16 00:10:58 +0800

    feat : a

--- src/main/java/com/qzx/xdupartner/entity/Result.java
+++ src/main/java/com/qzx/xdupartner/entity/Result.java
@@ -2,7 +2,9 @@ package com.qzx.xdupartner.entity;
 
 import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
 @AllArgsConstructor
 public class Result {
     WxMaJscode2SessionResult result;
