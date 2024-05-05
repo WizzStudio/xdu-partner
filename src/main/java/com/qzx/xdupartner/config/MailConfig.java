9e903b6cdd3707db70012ae0d443469928a88321
Qi-Zhen-xin <91938001+Qi-Zhen-xin@users.noreply.github.com>
2024-05-05 23:32:40 +0800

    Update MailConfig.java

--- src/main/java/com/qzx/xdupartner/config/MailConfig.java
+++ src/main/java/com/qzx/xdupartner/config/MailConfig.java
@@ -3,7 +3,6 @@ package com.qzx.xdupartner.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-
 import java.util.List;
 
 @Data
